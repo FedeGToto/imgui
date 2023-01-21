660a9ef1e114d5f9b323df286c78efe00637534f
Federico Giorgi <p2tv.live@gmail.com>
2023-01-21 02:34:06 +0100

    Fixed premake

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,9 @@
 project "ImGui"
-{
     kind "StaticLib"
     language "C++"
 
     targetdir ("bin/" .. outputdir .. "%{prj.name}")
-    objdir {"bin-int/" .. outputdir .. "%{prj.name}"}
+    objdir ("bin-int/" .. outputdir .. "%{prj.name}")
 
     files
     {
@@ -27,4 +26,3 @@ project "ImGui"
 
     filter {"system:windows", "configurations:Release"}
         buildoptions "/MT"
-}
